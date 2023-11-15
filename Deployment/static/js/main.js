0427a3b4e262f16c3d1fcfe2502d9eb3d73e2896
Ajay <88961945+AjayK47@users.noreply.github.com>
2023-11-15 09:06:11 +0530

    Add files via upload

---
+++ Deployment/static/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
+
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                // Display vitamins data
+                var vitaminsData = data[0];
+                for (var i = 0; i < vitaminsData.length; i++) {
+                    var item = vitaminsData[i];
+                    for (var key in item) {
+                        $('#result').append(key + ': ' + item[key] + '<br>');
+                    }
+                }
+
+                // Display facts
+                $('#result').append('<br>Fun Facts:<br>' + data[1]);
+                
+                console.log('Success!');
+            },
+        });
+    });
+});
